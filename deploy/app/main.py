b9903b1835e548f43a2d0eed709f17e4ff3b5b19
Ajay Chinta <ajay.chinta@gmail.com>
2019-08-23 08:38:04 +0530

    deploying starlette apps

---
+++ deploy/app/main.py
@@ -0,0 +1,10 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+app = Starlette()
+
+@app.route('/')
+async def homepage(request):
+    return JSONResponse({'message': "Hello World"})
+
+
